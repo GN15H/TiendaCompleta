666cced52bf1125c086b2c805b11c553b6b67e39
GN15H <cristobalochoacadavid@gmail.com>
2023-02-04 06:39:39 -0500

    fix-import

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,5 @@
 import express from 'express'
 import { bookProduct, getAllProducts, getProduct, updateProduct } from '../controllers/BlogController.js'
-import handler from '../controllers/CheckoutController.js'
 import { getAllImages, getImage } from '../controllers/ImageController.js'
 import { pay } from '../controllers/Payment.js'
 import { createUser, getAllUsers, getUser, updateUser } from '../controllers/UserController.js'
@@ -12,7 +11,6 @@ router.get('/users', getAllUsers)
 router.get('/images', getAllImages)
 router.get('/images/:id', getImage)
 router.get('/book/:id', bookProduct)
-router.post('/checkout', handler)
 router.post('/payment', pay)
 router.post('/users', getUser)
 router.post('/reg',createUser)
